4249a0c1dddeb4157de70b40fe3e8773c73915ab
Atef Hejazi <78237128+atefhejazi1@users.noreply.github.com>
2025-11-04 15:45:18 +0200

    make styles folder

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/shared/Hero";
 import ResumeSteps from "@/components/shared/resumeSteps/ResumeSteps";
-import Image from "next/image";
 
 export default function Home() {
   return (
